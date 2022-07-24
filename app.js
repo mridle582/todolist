17c53cbc015cbfe14ca497d5324c0540fad07737
mridle582 <mridle582@gmail.com>
2022-07-24 18:35:13 -0400

    add basic ejs

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    // res.send("Hello World");
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+
+    if (currentDay === 6 || currentDay === 0) {
+        day = "Weekend";
+    } else {
+        day = "Weekday";
+    }
+    res.render('list', { kindOfDay: day });
+
 });
 
 app.listen(3000, ()=>{
