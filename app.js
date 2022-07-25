2f17947a7df660b7b30913a856b14b9412a54f78
mridle582 <mridle582@gmail.com>
2022-07-25 16:22:11 -0400

    allow tasks to be entered and refactor

--- app.js
+++ app.js
@@ -3,45 +3,39 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var tasks = ["Buy Food","Cook Food", "Eat Food"];
+
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 app.get("/", (req, res) => {
-    // res.send("Hello World");
+
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
 
-    switch(currentDay) {
-        case 0: 
-            day="Sunday";
-            break;
-        case 1: 
-            day="Monday";
-            break;
-        case 2: 
-            day="Tuesday";
-            break;
-        case 3: 
-            day="Wednesday";
-            break;
-        case 4: 
-            day="Thursday";
-            break;
-        case 5: 
-            day="Friday";
-            break;
-        case 6: 
-            day="Saturday";
-            break;
-        default: 
-            console.log(`Invalid Value: currentDay= ${currentDay}`);
-    }
-    console.log(currentDay);
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    res.render('list', { kindOfDay: day });
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render('list', {
+        day: day,
+        tasks: tasks
+    });
 
 });
 
-app.listen(3000, ()=>{
+app.post("/", (req, res) => {
+    var task = req.body.newTask;
+    tasks.push(task);
+    res.redirect("/");
+});
+
+app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
